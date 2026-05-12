79dcb007548f3eb9da88f53029cc7389ca313a4d
Arga Asasta <asasta7@gmail.com>
2026-05-12 11:32:11 +0700

    Task/implement lottie animation qris (#24)
    * Add lottie for QR
    
    * delete unused
    
    * Update Version of Library
    
    * Fix review

--- components-sdk/src/main/java/co/xendit/components/ui/qrcode/QrPaymentUI.kt
+++ components-sdk/src/main/java/co/xendit/components/ui/qrcode/QrPaymentUI.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,20 +17,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import co.xendit.components.R
 import co.xendit.components.data.model.BffChannel
 import co.xendit.components.data.model.ChannelFormField
 import co.xendit.components.ui.style.xenditAppearance
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,6 +46,15 @@ internal fun QrPaymentUI(
   val appearance = xenditAppearance
   val formValues = remember { mutableStateMapOf<String, String>() }
   val visibleFields = remember { mutableStateOf<List<ChannelFormField>>(emptyList()) }
+
+  val composition by rememberLottieComposition(
+    LottieCompositionSpec.RawRes(R.raw.qr_scanner)
+  )
+
+  val progress by animateLottieCompositionAsState(
+    composition = composition,
+    iterations = LottieConstants.IterateForever // Loop it!
+  )
 
   LaunchedEffect(selectedChannel.channelCode) {
     formValues.clear()
@@ -62,26 +71,25 @@ internal fun QrPaymentUI(
         .padding(horizontal = 16.dp),
       color = appearance.colorBorder
     )
-    Spacer(modifier = Modifier.height(12.dp))
+    Spacer(modifier = Modifier.height(6.dp))
 
     Row(
       modifier = Modifier
         .fillMaxWidth()
-        .padding(horizontal = 20.dp, vertical = 8.dp),
+        .padding(horizontal = 16.dp, vertical = 8.dp),
       verticalAlignment = Alignment.CenterVertically
     ) {
-      Icon(
-        painter = painterResource(id = R.drawable.ic_qris),
-        contentDescription = null,
-        tint = appearance.colorTextSecondary,
-        modifier = Modifier.size(28.dp)
+      LottieAnimation(
+        composition = composition,
+        progress = { progress }, // Using a lambda for better performance
+        modifier = Modifier.size(60.dp)
       )
-      Spacer(modifier = Modifier.width(12.dp))
       Column(modifier = Modifier.weight(1f)) {
         selectedChannel.instructions?.forEachIndexed { index, instruction ->
           Text(
             text = instruction,
-            style = MaterialTheme.typography.titleSmall.takeIf { index == 0 } ?: MaterialTheme.typography.bodySmall,
+            style = MaterialTheme.typography.titleSmall.takeIf { index == 0 }
+              ?: MaterialTheme.typography.bodySmall,
             color = appearance.colorText.takeIf { index == 0 } ?: appearance.colorTextSecondary
           )
           if (index != selectedChannel.instructions.size - 1) {
