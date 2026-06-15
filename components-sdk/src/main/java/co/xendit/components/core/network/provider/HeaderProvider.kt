b3288146d0837bf6c083880d63e3946acba34480
Arga Asasta <asasta7@gmail.com>
2026-06-15 12:05:17 +0700

    remove header provider

--- components-sdk/src/main/java/co/xendit/components/core/network/provider/HeaderProvider.kt
+++ components-sdk/src/main/java/co/xendit/components/core/network/provider/HeaderProvider.kt
@@ -3,15 +3,10 @@ package co.xendit.components.core.network.provider
 import co.xendit.components.BuildConfig
 
 internal class HeaderProvider {
-  private var origin: String = "https://demo-store.xendit.co"
   private var merchantAppId: String = "co.example.components"
 
   fun setMerchantAppId(hostId: String) {
     this.merchantAppId = hostId
-  }
-
-  fun getOrigin(): String {
-    return origin
   }
 
   fun getSdkVersion(): String {
