9fc044e1a7a13433f6a8f9b006b984d32063df58
Arga Asasta <asasta7@gmail.com>
2026-06-25 14:35:32 +0700

    just remove the not supported

--- components-sdk/src/main/java/co/xendit/components/XenditComponents.kt
+++ components-sdk/src/main/java/co/xendit/components/XenditComponents.kt
@@ -38,6 +38,18 @@ enum class XenditComponentsPaymentType(val value: String) {
   @SerializedName("QR_CODE")
   QR_CODE("QR_CODE"),
 
+  @SerializedName("BANK_TRANSFER")
+  BANK_TRANSFER("BANK_TRANSFER"),
+
+  @SerializedName("DIRECT_DEBIT")
+  DIRECT_DEBIT("DIRECT_DEBIT"),
+
+  @SerializedName("VIRTUAL_ACCOUNT")
+  VIRTUAL_ACCOUNT("VIRTUAL_ACCOUNT"),
+
+  @SerializedName("OVER_THE_COUNTER")
+  OVER_THE_COUNTER("OVER_THE_COUNTER"),
+
   @FallbackValue
   @SerializedName("UNKNOWN")
   UNKNOWN("UNKNOWN");
