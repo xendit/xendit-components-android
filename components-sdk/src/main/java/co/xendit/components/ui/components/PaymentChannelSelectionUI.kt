efc74a40e5695c66534c4f8672596e255495a47a
Arga Asasta <asasta7@gmail.com>
2026-07-01 11:46:41 +0700

    Fix icon Tint with Text COlor

--- components-sdk/src/main/java/co/xendit/components/ui/components/PaymentChannelSelectionUI.kt
+++ components-sdk/src/main/java/co/xendit/components/ui/components/PaymentChannelSelectionUI.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -242,9 +243,10 @@ internal fun PaymentChannelSelectionUI(
       )
       Spacer(modifier = Modifier.height(8.dp))
       Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
+        Icon(
           painter = painterResource(R.drawable.img_phone_app),
           contentDescription = null,
+          tint = appearance.colorText,
           modifier = Modifier.size(60.dp)
         )
         Column {
